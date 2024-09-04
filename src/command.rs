94a68494c2b3a4d13864368557f7f44b384b2b77
Original Timi <timileyinoyelekan11@gmail.com>
2024-09-04 22:43:33 +0100

    refactor generate password

--- src/command.rs
+++ src/command.rs
@@ -1,18 +1,10 @@
 use crate::args;
-use crate::constants;
 use crate::utils;
 pub fn command(args:&args::Args){
     let _level = args.level;
     let mut password = String::new();
 
-
+    utils::generate_password(args.max, &mut password);
     // Generate password
-    let letters_index = utils::generate_rand(constants::LETTERS.len() as u16);
-    let numbers_index = utils::generate_rand(constants::NUMBERS.len() as u16);
-    let special_characters_index = utils::generate_rand(constants::SPECIAL_CHARACTERS.len() as u16);
-
-    password.push_str(&constants::LETTERS[letters_index as usize]);
-    password.push_str(&constants::NUMBERS[numbers_index as usize]);
-    password.push_str(&constants::SPECIAL_CHARACTERS[special_characters_index as usize]);
-    println!("Password: {}", password);
+    println!("Your Generated Password is\n {}", password);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,19 @@
+use crate::constants;
 use rand::Rng;
-pub fn generate_rand (length:u16)->u16{
+pub fn generate_rand(length: u16) -> u16 {
     let mut rng = rand::thread_rng();
     let random_index = rng.gen_range(0..length);
     random_index
+}
+
+pub fn generate_password(length: u16, s: &mut String) {
+    let _special_characters_index = generate_rand(constants::SPECIAL_CHARACTERS.len() as u16);
+    for _ in 0..length {
+        let letters_index = generate_rand(constants::LETTERS.len() as u16);
+        let numbers_index = generate_rand(constants::NUMBERS.len() as u16);
+
+        s.push_str(&constants::LETTERS[letters_index as usize]);
+        s.push_str(&constants::NUMBERS[numbers_index as usize]);
+    }
+    // s.push_str(&constants::SPECIAL_CHARACTERS[special_characters_index as usize]);
 }
